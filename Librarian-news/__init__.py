299226a53a4d6b398444d7aba36618c8f1101dbc
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-20 01:13:35 +0900

    refactor(main): Separate 'Librarian' and 'Librarian-news'
    refactor(main): Separate 'Librarian' and 'Librarian-news'

---
+++ Librarian-news/__init__.py
@@ -0,0 +1,12 @@
+"""
+Librarian specialized in collecting news data
+"""
+
+
+class Librarian_news:
+
+    def __init__(self, args, **kwargs):
+        super().__init__(args, **kwargs)
+        return
+
+
