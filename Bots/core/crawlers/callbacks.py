f74b39dcc12974237f4154456829e7f69336ade9
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-21 01:17:17 +0900

    feat(main): Create meta class for callbacks

--- Bots/core/crawlers/callbacks.py
+++ Bots/core/crawlers/callbacks.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+"""
+Callback functions used to process received data
+"""
+
+from abc import *
+
+
+class Callback(metaclass=ABCMeta):
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        pass
