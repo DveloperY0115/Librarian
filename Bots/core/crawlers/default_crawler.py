4b560f34f8a3e873274111e8ad79a20ea7b28472
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-21 18:18:35 +0900

    feat(main): Implement default crawler consists of basic callbacks

--- Bots/core/crawlers/default_crawler.py
+++ Bots/core/crawlers/default_crawler.py
@@ -9,18 +9,17 @@ Instance of this class is used in initialization of Spiders by default.
 from Bots.core.crawlers import Crawler
 from bs4 import BeautifulSoup
 
+from Bots.core.crawlers.callbacks import *
+
 
 class Default_Crawler(Crawler):
 
     def __init__(self, name='default_crawler'):
-        kwargs = {}
+        kwargs = {'callbacks': [get_http_header(), get_html()]}
         super().__init__(name, **kwargs)
 
     def __call__(self, http_response, **kwargs):
-        http_headers = http_response.headers
-        html = http_response.text
-        status = http_response.status_code
-
-        if not http_response.ok:
-            super().log("Something went wrong!")
-        return http_headers, html, status
+        result = {}
+        for callback in self.callbacks:
+            result[str(callback)] = callback(http_response)
+        return result
