5c9fcc5c08c7512d8892fd065c56692b3b176f99
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-21 16:44:55 +0900

    feat(main): Implement default crawler
    This simple crawler returns headers, HTML, and status code of the request sent by the spider it belongs to.

--- Bots/core/crawlers/default_crawler.py
+++ Bots/core/crawlers/default_crawler.py
@@ -7,6 +7,7 @@ Instance of this class is used in initialization of Spiders by default.
 """
 
 from Bots.core.crawlers import Crawler
+from bs4 import BeautifulSoup
 
 
 class Default_Crawler(Crawler):
@@ -16,4 +17,10 @@ class Default_Crawler(Crawler):
         super().__init__(name, **kwargs)
 
     def __call__(self, http_response, **kwargs):
-        return
+        http_headers = http_response.headers
+        html = http_response.text
+        status = http_response.status_code
+
+        if not http_response.ok:
+            super().log("Something went wrong!")
+        return http_headers, html, status
