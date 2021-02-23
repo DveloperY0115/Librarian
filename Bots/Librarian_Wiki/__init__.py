f7cd8638b3966fe09ce00f3ed6fd1c0699665d34
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-23 23:39:16 +0900

    feat(main): Implement crawler collecting title, text, edit history

--- Bots/Librarian_Wiki/__init__.py
+++ Bots/Librarian_Wiki/__init__.py
@@ -4,21 +4,34 @@ from scrapy.spiders import CrawlSpider
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 
+
 class ArticleSpider(CrawlSpider):
     name = 'Wiki_Articles'
     allowed_domains = ['wikipedia.org']
     start_urls = [
-        'https://en.wikipedia.org/wiki/Python_(programming_language)'
+        'https://en.wikipedia.org/wiki/Computer'
     ]
-    rules = [Rule(link_extractor=LinkExtractor(allow=r'.*'), callback='parse')]
+    rules = [
+        Rule(link_extractor=LinkExtractor(allow='en.wikipedia.org/wiki/((?!:).)*$'),
+             callback='parse', follow=False,
+             cb_kwargs={'is_article': True}),
+        Rule(LinkExtractor(allow='.*'), callback='parse', follow=False,
+             cb_kwargs={'is_article': False})
+    ]
 
-    def parse(self, response):
-        url = response.url
-        title = response.css('h1::text').extract_first()
+    def parse(self, response, is_article):
+        print(response.url)
+        title = response.xpath('//title//text()').get()
 
-        print('URL: {}'.format(url))
-        print('Title: {}'.format(title))
-
-        next_page = response.css('li.next a::attr("href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        if is_article:
+            url = response.url
+            text = response.xpath('//div[@id="mw-content-text"]//p/*').getall()
+            last_updated = response.xpath('//li[@id="footer-info-lastmod"]//text()').get()
+            last_updated = last_updated.replace('This page was last edited on ', '')
+            
+            print(url)
+            print('Title: {}'.format(title))
+            print('last updated: {}'.format(last_updated))
+            print('Text: {}'.format(text))
+        else:
+            print('This is not an article: {}'.format(title))
