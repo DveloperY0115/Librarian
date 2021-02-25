a1cdbdde739d43a3bce58546e98397c4c93b662d
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-25 15:58:06 +0900

    refactor(main): Remove 'setup.py'

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='Librarian',
-      version='0.1',
-      description='Set of spiders used to collect data for search engine, Alexandria.',
-      license='MIT',
-      packages=['Bots'],
-      zip_safe=False)
