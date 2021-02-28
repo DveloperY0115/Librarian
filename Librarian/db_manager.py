e5d116b3d74e5ce9cdd9eeb4817931f0fb556fac
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-28 17:43:46 +0900

    refactor(main): Separate function 'init_connection' from constructor

--- Librarian/db_manager.py
+++ Librarian/db_manager.py
@@ -7,26 +7,9 @@ class DatabaseManager:
         self.db = db
         self.user = user
         self.host = host
-
-        def init_connection():
-            try:
-                conn = pymysql.connect(db=self.db,
-                                       user=self.user, passwd=passwd,
-                                       host=self.host,
-                                       charset='utf8', use_unicode=True)
-                cursor = conn.cursor()
-            except pymysql.Error as e:
-                err_code, msg = e.args
-                print("[{0}]: {1}".format(err_code, msg))
-                raise SystemExit
-
-            print("Connected to Database: {0}, hosted on: {1}, as user name: {2}".format(db, host, user))
-            return conn, cursor
-
-        self.conn, self.cursor = init_connection()
+        self.conn, self.cursor = self.init_connection(db, user, passwd, host)
 
     def register_item(self, item, table, overwrite=True):
-
         if self.check_exists(item, {'url': item.get('url')}, table):
             # if the page of the same URL already exists in the table
             if overwrite:
@@ -40,10 +23,26 @@ class DatabaseManager:
                                     ))
                 self.conn.commit()
             else:
+                # Do nothing.
                 pass
 
     def check_exists(self, item, field_dict, table):
         return True
+
+    def init_connection(self, db, user, passwd, host):
+        try:
+            conn = pymysql.connect(db=db,
+                                   user=user, passwd=passwd,
+                                   host=host,
+                                   charset='utf8', use_unicode=True)
+            cursor = conn.cursor()
+        except pymysql.Error as e:
+            err_code, msg = e.args
+            print("[{0}]: {1}".format(err_code, msg))
+            raise SystemExit
+
+        print("Connected to Database: {0}, hosted on: {1}, as user name: {2}".format(db, host, user))
+        return conn, cursor
 
     def close_connection(self):
         try:
